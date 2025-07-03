79bf7bf6fc55354d6f99bda65bdc4cc91f14d286
Kristian Lazaric <lazarickristian@gmail.com>
2025-07-03 13:44:07 +0200

    Little changes in templates and settings.py
    Little changes in templates and settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^bq*m@po5zollmwwnuuk_nf(pl(tvrf)j@==oz*u@61f02l@-a'
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
